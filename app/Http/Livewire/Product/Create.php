8303c29ec0f51be95464ae6f4297e42fa588d578
willywin99 <willywinsianturi@gmail.com>
2022-06-14 05:10:13 +0700

    Menyimpan Data Product secara Realtime

--- app/Http/Livewire/Product/Create.php
+++ app/Http/Livewire/Product/Create.php
@@ -4,10 +4,28 @@ namespace App\Http\Livewire\Product;
 
 use Livewire\Component;
 
+use App\Product;
+
 class Create extends Component
 {
+    public $title;
+    public $description;
+    public $price;
+    public $image;
+
     public function render()
     {
         return view('livewire.product.create');
+    }
+
+    public function store()
+    {
+        Product::create([
+            'title' => $this->title,
+            'description' => $this->description,
+            'price' => $this->price,
+        ]);
+
+        $this->emit('productStored');
     }
 }
